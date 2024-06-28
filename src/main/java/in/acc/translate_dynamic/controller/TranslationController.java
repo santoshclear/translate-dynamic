a95b3cac19e9ebd49045a6a77076f6a88916246c
Gudikandula <sai.sree.gudikandula@accenture.com>
2024-06-28 16:42:30 +0530

    new

--- src/main/java/in/acc/translate_dynamic/controller/TranslationController.java
+++ src/main/java/in/acc/translate_dynamic/controller/TranslationController.java
@@ -1,16 +1,24 @@
 package in.acc.translate_dynamic.controller;
 
+import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.PDResources;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
+import org.apache.pdfbox.pdmodel.graphics.PDXObject;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.apache.pdfbox.text.PDFTextStripper;
-import org.apache.poi.xwpf.usermodel.*;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.geom.Rectangle2D;
 import java.io.*;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -60,27 +68,40 @@ public class TranslationController {
              PDDocument document = PDDocument.load(inputStream)) {
 
             PDDocument translatedDocument = new PDDocument();
-            PDType0Font font = PDType0Font.load(translatedDocument, new File("C:\\path\\to\\your\\font.ttf"));
+            PDType0Font font = PDType0Font.load(translatedDocument, new File("C:\\Users\\sai.sree.gudikandula\\OneDrive - Accenture\\Desktop\\python\\Noto_Sans\\static\\NotoSans_SemiCondensed-SemiBoldItalic.ttf"));
 
             PDFTextStripper textStripper = new PDFTextStripper();
             textStripper.setSortByPosition(true);
 
             for (int page = 1; page <= document.getNumberOfPages(); ++page) {
+                PDPage originalPage = document.getPage(page - 1);
+                PDPage translatedPage = new PDPage(originalPage.getMediaBox());
+                translatedDocument.addPage(translatedPage);
+
                 textStripper.setStartPage(page);
                 textStripper.setEndPage(page);
 
                 String originalText = textStripper.getText(document);
                 String translatedText = translateTextUsingPython(originalText, "en", targetLang);
 
-                PDPage translatedPage = new PDPage(document.getPage(page - 1).getMediaBox());
-                translatedDocument.addPage(translatedPage);
-
-                PDPageContentStream contentStream = new PDPageContentStream(translatedDocument, translatedPage);
+                PDPageContentStream contentStream = new PDPageContentStream(translatedDocument, translatedPage, PDPageContentStream.AppendMode.APPEND, true, true);
                 contentStream.setFont(font, 12);
+
+                // Using text extraction by area to preserve formatting
+                PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+                stripper.setSortByPosition(true);
+
+                // Define the region you want to extract text from
+                Rectangle2D region = new Rectangle2D.Double(0, 0, originalPage.getMediaBox().getWidth(), originalPage.getMediaBox().getHeight());
+                stripper.addRegion("region", region);
+                stripper.extractRegions(originalPage);
+
+                String regionText = stripper.getTextForRegion("region");
+                String[] lines = regionText.split("\n");
+
                 contentStream.beginText();
                 contentStream.newLineAtOffset(50, 700);
 
-                String[] lines = translatedText.split("\n");
                 for (String line : lines) {
                     contentStream.showText(line);
                     contentStream.newLineAtOffset(0, -15); // Adjust the offset for each new line
@@ -88,6 +109,18 @@ public class TranslationController {
 
                 contentStream.endText();
                 contentStream.close();
+
+                // Copying images and other graphics
+                PDResources pageResources = originalPage.getResources();
+                for (COSName xObjectName : pageResources.getXObjectNames()) {
+                    PDXObject xObject = pageResources.getXObject(xObjectName);
+                    if (xObject instanceof PDImageXObject) {
+                        PDImageXObject image = (PDImageXObject) xObject;
+                        PDPageContentStream imageContentStream = new PDPageContentStream(translatedDocument, translatedPage, PDPageContentStream.AppendMode.APPEND, true, true);
+                        imageContentStream.drawImage(image, image.getWidth(), image.getHeight());
+                        imageContentStream.close();
+                    }
+                }
             }
 
             translatedDocument.save("translated.pdf");
